16e86507ca525af4532ebc22410fd05e361b86d5
Juan Alvarado <jalvarado91@gmail.com>
2018-03-04 02:22:14 -0500

    shell

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { url_value: "", loading: false, video_url: null };
+  }
+
+  handleChange = event => {
+    this.setState({
+      url_value: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    console.log(this.state.url_value);
+  };
+
   render() {
+    const { video_url, url_value, loading } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>Tastai</h1>
+
+          <div>
+            <label htmlFor="url-input">
+              Give us a long boring cooking video and we'll make it delicious
+              for you
+            </label>
+            <br />
+            <br />
+            <input
+              id="url-input"
+              value={url_value}
+              onChange={this.handleChange}
+              placeholder="http://youtube.com?v=someVideoUrl"
+            />
+            <button onClick={this.handleSubmit}>Tastaify!</button>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {video_url != null ? <div>Video Here</div> : ""}
       </div>
     );
   }
